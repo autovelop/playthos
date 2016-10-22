2175e2767f1fb67c90f0f66e12fdb82ff9fcda7c
Fanus <fanus.link@gmail.com>
2016-10-22 18:17:33 +0200

    Updated build file sfor linux and test. Also properly formatted the files for vim

--- src/shared/shared.go
+++ src/shared/shared.go
@@ -4,7 +4,7 @@ const VSHADER_OPENGL_4_1 = `#version 410
 in vec2 position;
 void main()
 {
-    gl_Position = vec4(position, 0.0, 1.0);
+  gl_Position = vec4(position, 0.0, 1.0);
 }
 ` + "\x00"
 
@@ -12,7 +12,7 @@ const FSHADER_OPENGL_4_1 = `#version 410
 out vec4 outColor;
 void main()
 {
-    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+  outColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
 ` + "\x00"
 
@@ -20,13 +20,13 @@ const VSHADER_OPENGL_ES_2_0 = `#version 100
 
 attribute vec4 position;
 void main() {
-	gl_Position = position;
+  gl_Position = position;
 }`
 
 const FSHADER_OPENGL_ES_2_0 = `#version 100
 precision mediump float;
 void main() {
-	gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }`
 
 
@@ -79,16 +79,16 @@ uniform vec2 offset;
 
 attribute vec4 position;
 void main() {
-	// offset comes in with x/y values between 0 and 1.
-	// position bounds are -1 to 1.
-	vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
-	gl_Position = position + offset4;
+  // offset comes in with x/y values between 0 and 1.
+  // position bounds are -1 to 1.
+  vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
+  gl_Position = position + offset4;
 }`
 
 const FragmentShaderMob = `#version 100
 precision mediump float;
 uniform vec4 color;
 void main() {
-	gl_FragColor = color;
+  gl_FragColor = color;
 }
 `
