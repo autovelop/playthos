d8ef86ef638ebdb1ec97283f9afda1b6c2d831d0
Fanus <fanus@autovelop.com>
2018-07-29 13:48:47 +0200

    removed tests

--- tests/run_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export GOCACHE=off
-# go test -v  headless/headless_test.go
-# go test -v -tags="render glfw" glfw/glfw_test.go
-# go test -v -tags="render glfw opengl web deploy" opengl/opengl_texture_test.go
-go run -v -tags="render glfw opengl deploy" opengl/opengl_texture.go
-# go test -v -tags="render glfw opengl" opengl/opengl_test.go
